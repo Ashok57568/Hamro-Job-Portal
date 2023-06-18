8cde50ed25b7eb8699a7b9c6018059a96c8755ec
Ashok Dhungana <you@example.com>
2023-06-18 09:48:44 +0545

    Initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+//imports express
+//const express = require('express')
+import express from "express";
+import dotenv from "dotenv";
+import colors from  "colors";
+import connectDB from "./config/db.js";
+import testRoutes from './routes/testRoutes.js'
+//rest object
+
+//Dot ENV config
+dotenv.config();
+
+//mongodb connection
+connectDB();
+
+const app = express()
+
+//routes
+app.use('/api/v1/test',testRoutes)
+
+//port
+const PORT = process.env.PORT ||8080
+
+
+//listen
+app.listen(PORT,()=>{
+    console.log(`Node Server Running In ${process.env.DEV_MODE} Mode on  port no ${PORT}`.bgCyan.white);
+})
+
+ 
